66138cabb373a529cd786bbf473ad764dd874446
unknown <masumaakter.a1@gmail.com>
2024-03-12 15:07:55 +0600

    Bootstrap-Portfolio

--- Event-JavaScript/script.js
+++ Event-JavaScript/script.js
@@ -1,3 +1,37 @@
+function Portfolio(){
+    fetch("portfolio.json")
+    .then(res => res.json())
+    .then(data => displayPortfolio(data))
+
+}
+Portfolio();
+const displayPortfolio = (data) =>{
+    const allportfolio = document.getElementById('Portfolio');
+    for(const portfolio of data){
+        const div = document.createElement('div');
+        div.classList.add("portfolio")  
+         div.innerHTML = `
+        
+        <div class="">
+        <img src="images/3.jpg" class="card-img-top" alt="...">
+        </div>
+        `;
+    
+        allportfolio.appendChild(div);
+        console.log(portfolio);
+        }
+        }
+        
+
+
+
+
+
+
+
+
+
+//----------------form area start----------------//
 const form = document.querySelector("form");
 const Yourname = document.querySelector("#type");
 const Email = document.querySelector("#E-mail");
@@ -20,3 +54,4 @@ function formHandler(e){
     console.log(forminfo);
     
 }
+//----------------form area end----------------//
